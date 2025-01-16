0008b4e259aea017189c54deca93ae17d4c07ab1
cayleb247 <156046197+cayleb247@users.noreply.github.com>
2025-01-16 00:42:25 -0500

    Add script to get drug compound data from ChEMBL

---
+++ Drugs_Proteins/chembl_api.py
@@ -0,0 +1,81 @@
+import requests
+
+
+def getDrugIds(drug_list: list):
+    '''
+    Use ChEMBL API to get ChEMBL id for each drug of a list
+
+    Parameters:
+    drug_list (list): an input list of drugs
+
+    Returns:
+    list: a list of entry dictionaries with drug name and ID
+    '''
+
+    drug_data = []
+
+    for drug in drug_list:
+
+        # ChEMBL API URL for molecule search
+        url = f"https://www.ebi.ac.uk/chembl/api/data/molecule/search?q={drug}"
+
+        headers = {
+            "Accept": "application/json"
+        }
+
+        response = requests.get(url, headers=headers)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()  # Parse the JSON response
+
+            if data['molecules']:
+                first_result = data['molecules'][0] # Get only the first entry's data
+
+                name = first_result.get('pref_name', 'No name available')
+                if name == None: # Check if name is unavailable
+                    name = f"{drug.upper()}*" # Use input name and mark with asterisk
+
+                chembl_id = first_result['molecule_chembl_id']
+
+                entry = {
+                    "name": name,
+                    "chembl_id": chembl_id
+                }
+                drug_data.append(entry)
+            else:
+                print(f"No compounds found for {drug}")
+        else:
+            print(f"Failed to fetch data: {response.status_code}")
+
+    return drug_data
+
+def getDrugSMILES(drug_list):
+
+    for drug in drug_list:
+        url = f"https://www.ebi.ac.uk/chembl/api/data/molecule/{drug['chembl_id']}.json"
+
+        # Send GET request to fetch compound data
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            smiles = data.get("molecule_structures", {}).get("canonical_smiles", "No SMILES found")
+            drug["SMILES"] = smiles
+            
+        else:
+            print("Error fetching data from ChEMBL API")
+    
+    return drug_list
+
+
+def getDrugData(drug_list: list):
+    '''
+    Get a list of dictionary entries comprised of drug name, id, and SMILES string
+    '''
+
+    drug_ids = getDrugIds(drug_list)
+
+    drug_smiles = getDrugSMILES(drug_ids)
+
+    return(drug_smiles)
